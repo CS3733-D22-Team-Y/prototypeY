bd82707cc93002e407169dcc8a9e184eb0eca391
emgore1016 <51887594+emgore1016@users.noreply.github.com>
2022-03-28 20:53:26 -0400

    started linking different scenes

--- src/main/java/edu/wpi/cs3733/d22/teamY/controllers/mainPageController.java
+++ src/main/java/edu/wpi/cs3733/d22/teamY/controllers/mainPageController.java
@@ -2,23 +2,38 @@ package edu.wpi.cs3733.d22.teamY.controllers;
 
 import edu.wpi.cs3733.d22.teamY.App;
 import java.io.IOException;
+import java.util.Objects;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 
 public class mainPageController {
   @FXML
   void backToWelcomePage(ActionEvent event) throws IOException {
     // Setting it to request menu for now but will make it to welcome page
-    App.getInstance().setSceneToRequestMenu();
+    Scene s =
+        new Scene(
+            FXMLLoader.load(
+                Objects.requireNonNull(getClass().getResource("views/requestMenu.fxml"))));
+    App.getInstance().setScene(s);
   }
 
-  void createServiceRequest(ActionEvent event) throws IOException{
-    //Sets scene to service request page
-    App.getInstance().setSceneToRequestMenu();
+  void createServiceRequest(ActionEvent event) throws IOException {
+    // Sets scene to service request page
+    Scene s =
+        new Scene(
+            FXMLLoader.load(
+                Objects.requireNonNull(App.class.getResource("views/requestMenu.fxml"))));
+    App.getInstance().setScene(s);
   }
 
-  void viewServiceRequest(ActionEvent event) throws IOException{
-    //Sets scene to view service request
-    App.getInstance().setSceneToRequestMenu();
+  void viewServiceRequest(ActionEvent event) throws IOException {
+    // Sets scene to view service request
+    Scene s =
+        new Scene(
+            FXMLLoader.load(
+                Objects.requireNonNull(App.class.getResource("views/activeRequests.fxml"))));
+    App.getInstance().setScene(s);
   }
 }
